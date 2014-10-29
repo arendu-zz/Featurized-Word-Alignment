the gradient seems to be correct now

DifferentiableFunction.py
@@ -58,7 +58,10 @@ class DifferentiableFunction:
         from scipy.optimize import approx_fprime
 
         eps = np.sqrt(np.finfo(np.float).eps)
-        return approx_fprime(initials, self.value_translator, [eps] * self.size)
+        approx_fprime = approx_fprime(initials, self.value_translator, [eps] * self.size)
+        for i, x in enumerate(approx_fprime):
+            theta[self.index2feature[i]] = x
+        return theta
 
 
     def finite_diff(self, theta):
@@ -158,7 +161,7 @@ class DifferentiableFunction:
             from scipy.optimize import fmin_l_bfgs_b
             # http://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin_l_bfgs_b.html
             (xopt, fopt, return_status) = fmin_l_bfgs_b(self.value_translator, initials, self.gradient_translator,
-                                                        pgtol=0.1)
+                                                        pgtol=1.0)
             # print "============Optimization by LBFGS returns: ", return_status['task']
         elif self.method == "CG":
             from scipy.optimize import fmin_cg

featurized_em_wa.py
@@ -1,6 +1,7 @@
 __author__ = 'arenduchintala'
 
 from optparse import OptionParser
+import sys
 from math import exp, log
 from DifferentiableFunction import DifferentiableFunction
 import numpy as np
@@ -15,8 +16,8 @@ import itertools
 
 global BOUNDARY_STATE, END_STATE, SPLIT, E_TYPE, T_TYPE, possible_states, normalizing_decision_map
 global cache_normalizing_decision, features_to_conditional_arcs, conditional_arcs_to_features
-global trellis, span_limit
-span_limit = 10  # creates a span of +/- span centered around current token
+global trellis, max_jump_width
+max_jump_width = 4  # creates a span of +/- span centered around current token
 trellis = []
 cache_normalizing_decision = {}
 BOUNDARY_STATE = "###"
@@ -38,6 +39,15 @@ possible_obs = {}
 normalizing_decision_map = {}
 
 
+def get_jump(aj, aj_1):
+    if aj != NULL and aj_1 != 'NULL':
+        jump = abs(aj_1 - aj)
+        assert jump <= max_jump_width + 1
+    else:
+        jump = NULL
+    return jump
+
+
 def populate_features():
     global trellis, feature_index
     for treli in trellis:
@@ -54,22 +64,17 @@ def populate_features():
                         """
                         transition features
                         """
-                        if s_idx != NULL and prev_s_idx != 'NULL':
-                            jump = abs(prev_s_idx - s_idx)
-                        else:
-                            jump = NULL
-
-                        if jump == NULL or jump < span_limit:
-                            ff_t = FE.get_wa_features_fired(type=T_TYPE, decision=s_idx, context=(prev_s_idx, L))
-                            transition_arc = (s_idx, (prev_s_idx, L))
-                            for f in ff_t:
-                                feature_index[f] = feature_index.get(f, 0) + 1
-                                ca2f = conditional_arcs_to_features.get(transition_arc, set([]))
-                                ca2f.add(f)
-                                conditional_arcs_to_features[transition_arc] = ca2f
-                                f2ca = features_to_conditional_arcs.get(f, set([]))
-                                f2ca.add(transition_arc)
-                                features_to_conditional_arcs[f] = f2ca
+                        jump = get_jump(s_idx, prev_s_idx)
+                        ff_t = FE.get_wa_features_fired(type=T_TYPE, decision=jump, context=prev_s_idx)
+                        transition_arc = (jump, prev_s_idx)
+                        for f in ff_t:
+                            feature_index[f] = feature_index.get(f, 0) + 1
+                            ca2f = conditional_arcs_to_features.get(transition_arc, set([]))
+                            ca2f.add(f)
+                            conditional_arcs_to_features[transition_arc] = ca2f
+                            f2ca = features_to_conditional_arcs.get(f, set([]))
+                            f2ca.add(transition_arc)
+                            features_to_conditional_arcs[f] = f2ca
 
                 """
                 emission features
@@ -87,7 +92,7 @@ def populate_features():
 
 
 def populate_normalizing_terms(target):
-    global span_limit
+    global max_jump_width
     for treli in trellis:
         for idx in treli:
             tar = target[idx]
@@ -97,23 +102,13 @@ def populate_normalizing_terms(target):
                 ndm = normalizing_decision_map.get((E_TYPE, s_tok), set([]))
                 ndm.add(t_tok)
                 normalizing_decision_map[E_TYPE, s_tok] = ndm
-                # TODO: get transition/distortion parameters
-                # TODO: we dont need to explicitly define the normalizing decisions for transition
-                # TODO: because for every jump there are 0-span_limit possible jumps
-                if idx > 0:
-                    for prev_t_idx, prev_t_tok, prev_s_idx, prev_s_tok, L in treli[idx - 1]:
-                        """
-                        transition features
-                        """
-                        if s_idx != NULL and prev_s_idx != NULL:
-                            ndm = normalizing_decision_map.get((T_TYPE, (prev_s_idx, L)), set([]))
-                            ndm.add(abs(prev_s_idx - s_idx))
-                            normalizing_decision_map[T_TYPE, (prev_s_idx, L)] = ndm
-                        elif prev_s_idx == NULL:
-                            jump = range(span_limit) + [NULL]
-                            ndm = normalizing_decision_map.get((T_TYPE, (prev_s_idx, L)), set([]))
-                            ndm.update(jump)
-                            normalizing_decision_map[T_TYPE, (prev_s_idx, L)] = ndm
+
+                if s_idx == 0:
+                    normalizing_decision_map[T_TYPE, s_idx] = set(range(max_jump_width) + [NULL])
+                elif s_idx == L:
+                    pass
+                else:
+                    normalizing_decision_map[T_TYPE, s_idx] = set(range(max_jump_width) + [NULL])
 
 
 def get_transition_no_feature(jump):
@@ -134,14 +129,15 @@ def get_decision_given_context(theta, type, decision, context):
         theta_dot_normalizing_features = float('-inf')
         for d in normalizing_decisions:
             d_features = FE.get_wa_features_fired(type=type, context=context, decision=d)
-            try:
-                theta_dot_normalizing_features = utils.logadd(theta_dot_normalizing_features,
-                                                              sum([theta[f] for f in d_features if f in theta]))
-            except OverflowError:
-                pdb.set_trace()
+            theta_dot_normalizing_features = utils.logadd(theta_dot_normalizing_features,
+                                                          sum([theta[f] for f in d_features if f in theta]))
+
         cache_normalizing_decision[type, context] = theta_dot_normalizing_features
     log_prob = theta_dot_features - theta_dot_normalizing_features
-    return log_prob  # log(prob)
+    if log_prob > 0.0:
+        print log_prob, type, decision, context
+        raise Exception
+    return log_prob
 
 
 def get_possible_states(o):
@@ -160,22 +156,16 @@ def get_backwards(theta, obs, alpha_pi):
     S = alpha_pi[(n, COMPOSITE_BOUNDARY_STATE)]  # from line 13 in pseudo code
     for k in range(n, 0, -1):
         for v in obs[k]:
-            tk, t_tok, aj, sj, L = v
-            e = get_decision_given_context(theta, E_TYPE, decision=t_tok, context=sj)
+            tk, t_tok, aj, s_tok, L = v
+            e = get_decision_given_context(theta, E_TYPE, decision=t_tok, context=s_tok)
             pb = beta_pi[(k, v)]
-            # accumulate_fc(type=S_TYPE, alpha=alpha_pi[(k, v)], beta=beta_pi[k, v], k=k, S=S, d=t_tok)
-            accumulate_fc(type=E_TYPE, alpha=alpha_pi[(k, v)], beta=beta_pi[k, v], S=S, d=t_tok, c=sj)
+            accumulate_fc(type=E_TYPE, alpha=alpha_pi[(k, v)], beta=beta_pi[k, v], S=S, d=t_tok, c=s_tok)
             for u in obs[k - 1]:
-                tk_1, t_tok_1, aj_1, sj_1, L = u
-                q = get_decision_given_context(theta, T_TYPE, decision=aj,
-                                               context=(aj_1, L))
-                # print 'q_new', q
-                # try:
-                # q = get_transition_no_feature(aj - aj_1)
-                # print 'q_old', q
-                # except TypeError:
-                # q = log(1.0 / len(obs[k]))
+                tk_1, t_tok_1, aj_1, s_tok_1, L = u
 
+                jump = get_jump(aj, aj_1)
+                q = get_decision_given_context(theta, T_TYPE, decision=jump, context=aj_1)
+                # q = log(1.0 / len(obs[k]))
                 p = q + e
                 beta_p = pb + p  # The beta includes the emission probability
                 new_pi_key = (k - 1, u)
@@ -185,13 +175,13 @@ def get_backwards(theta, obs, alpha_pi):
                     beta_pi[new_pi_key] = utils.logadd(beta_pi[new_pi_key], beta_p)
                 # print 'beta     ', new_pi_key, '=', beta_pi[new_pi_key], exp(beta_pi[new_pi_key])
                 # alpha_pi[(k - 1, u)] + p + beta_pi[(k, v)] - S
-                accumulate_fc(type=T_TYPE, alpha=alpha_pi[k - 1, u], beta=beta_pi[k, v], q=q, e=e, d=aj, c=(aj_1, L),
+                accumulate_fc(type=T_TYPE, alpha=alpha_pi[k - 1, u], beta=beta_pi[k, v], q=q, e=e, d=jump, c=aj_1,
                               S=S)
     return S, beta_pi
 
 
 def get_viterbi_and_forward(theta, obs):
-    global span_limit
+    global max_jump_width
     COMPOSITE_BOUNDARY_STATE = obs[0][0]
     pi = {(0, COMPOSITE_BOUNDARY_STATE): 0.0}
     alpha_pi = {(0, COMPOSITE_BOUNDARY_STATE): 0.0}
@@ -201,15 +191,14 @@ def get_viterbi_and_forward(theta, obs):
             max_prob_to_bt = {}
             sum_prob_to_bt = []
             for u in obs[k - 1]:  # [1]:
-                tk, t_tok, aj, sj, L = v
-                tk_1, t_tok_1, aj_1, sj_1, L = u
-                q = get_decision_given_context(theta, T_TYPE, decision=aj,
-                                               context=(aj_1, L))
-                # try:
-                # q = get_transition_no_feature(aj - aj_1)
-                # except TypeError:
+                tk, t_tok, aj, s_tok, L = v
+                tk_1, t_tok_1, aj_1, s_tok_1, L = u
+
+                jump = get_jump(aj, aj_1)
+                q = get_decision_given_context(theta, T_TYPE, decision=jump, context=aj_1)
+
                 # q = log(1.0 / len(obs[k]))
-                e = get_decision_given_context(theta, E_TYPE, decision=t_tok, context=sj)
+                e = get_decision_given_context(theta, E_TYPE, decision=t_tok, context=s_tok)
                 p = pi[(k - 1, u)] + q + e
                 alpha_p = alpha_pi[(k - 1, u)] + q + e
                 if len(arg_pi[(k - 1, u)]) == 0:
@@ -280,7 +269,7 @@ def get_likelihood(theta):
         data_likelihood += S
 
     reg = sum([t ** 2 for k, t in theta.items()])
-    print 'accumulating', data_likelihood - (0.5 * reg)  # , ' '.join(obs)
+    print 'reg log likelihood:', data_likelihood - (0.5 * reg)  # , ' '.join(obs)
     return data_likelihood - (0.5 * reg)
 
 
@@ -295,7 +284,8 @@ def get_gradient(theta):
             fractional_count_grad[type, d, c] = Adc * (1 - a_dc)
         elif type == T_TYPE:
             Adc = exp(fractional_counts.get(event, float('-inf')))
-            a_dc = exp(get_decision_given_context(theta, type=T_TYPE, decision=d, context=c))
+            dd = get_decision_given_context(theta, type=T_TYPE, decision=d, context=c)
+            a_dc = exp(dd)
             fractional_count_grad[type, d, c] = Adc * (1 - a_dc)
 
     grad = {}
@@ -313,11 +303,12 @@ def get_gradient(theta):
             grad[t] = 0.0 - (0.5 * theta[t])
         else:
             grad[t] -= (0.5 * theta[t])
+    print 'max gradient      :', max(grad.values())
     return grad
 
 
 def populate_trellis(source_corpus, target_corpus):
-    global trellis, span_limit
+    global trellis, max_jump_width
     for s_sent, t_sent in zip(source_corpus, target_corpus):
         t_sent.insert(0, BOUNDARY_STATE)
         s_sent.insert(0, BOUNDARY_STATE)
@@ -328,8 +319,9 @@ def populate_trellis(source_corpus, target_corpus):
             if t_tok == BOUNDARY_STATE:
                 current_trellis[t_idx] = [(t_idx, BOUNDARY_STATE, t_idx, BOUNDARY_STATE, len(s_sent))]
             else:
-                start = t_idx - span_limit if t_idx - span_limit >= 0 else 0
-                end = t_idx + span_limit + 1
+                start = t_idx - (max_jump_width / 2) if t_idx - (max_jump_width / 2) >= 0 else 0
+                end = t_idx + (max_jump_width / 2) + 1
+                assert end - start <= max_jump_width + 1
                 current_trellis[t_idx] = [(t_idx, t_tok, s_idx + start, s_tok, len(s_sent)) for s_idx, s_tok in
                                           enumerate(s_sent[start:end]) if s_tok != BOUNDARY_STATE] + [
                                              (t_idx, t_tok, NULL, NULL, len(s_sent))]
@@ -341,8 +333,8 @@ if __name__ == "__main__":
     possible_states = defaultdict(set)
     possible_obs = defaultdict(set)
     opt = OptionParser()
-    opt.add_option("-t", dest="target_corpus", default="data/toy2/en")
-    opt.add_option("-s", dest="source_corpus", default="data/toy2/fr")
+    opt.add_option("-t", dest="target_corpus", default="data/small/en20.50")
+    opt.add_option("-s", dest="source_corpus", default="data/small/es20.50")
     opt.add_option("-o", dest="save", default="theta.out")
     opt.add_option("-a", dest="alignments", default="alignments.out")
     (options, _) = opt.parse_args()
@@ -352,14 +344,20 @@ if __name__ == "__main__":
     populate_features()
     populate_normalizing_terms(target)
     init_theta = dict((k, 1.0) for k in feature_index)
+
     # init_theta = dict((k, np.random.uniform(0, 0.1)) for k in feature_index)
     F = DifferentiableFunction(get_likelihood, get_gradient)
+    F.method = "LBFGS"
     (fopt, theta, return_status) = F.maximize(init_theta)
-
+    # print chk_grad
+    # chk_grad = F.fprime(init_theta)
+    # for k in sorted(theta):
+    # print theta[k], chk_grad[k], k
     # print return_status
     write_theta = open(options.save, 'w')
     for t in sorted(theta):
-        write_theta.write('\t'.join(t) + "\t" + str(theta[t]) + '' + "\n")
+        str_t = reduce(lambda a, d: str(a) + '\t' + str(d), t, '')
+        write_theta.write(str_t.strip() + str(theta[t]) + '' + "\n")
     write_theta.flush()
     write_theta.close()
 