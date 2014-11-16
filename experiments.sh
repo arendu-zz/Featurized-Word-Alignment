07dc3443b9eda41ea56e690588b35efb7fad3416
arendu <adithya.r@gmail.com>
2014-11-16 17:15:48 -0500

    check convergence sgd and lbfgs experiments

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.model1.full.txt
+(time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.parallel.model1.full.txt
+
 (time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.model1.full.txt
 (time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.parallel.model1.full.txt
+
+(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.model1.full.txt
+(time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.parallel.model1.full.txt
