b52f689009cc720307891cc9ad27028f337ed788
arendu <adithya.r@gmail.com>
2014-11-16 17:18:49 -0500

    more exp

--- experiments.sh
+++ experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.model1.full.txt
-(time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.parallel.model1.full.txt
+(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.lbfgs.model1.100.txt
+(time python featurized_em_wa.py -a SGD -m model1 -s data/small/es20.100 -t data/small/en20.100) &> log.sgd.model1.100.txt
 
-(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.model1.full.txt
-(time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.parallel.model1.full.txt
+(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.lbfgs.model1.200.txt
+(time python featurized_em_wa.py -a SGD -m model1 -s data/small/es20.200 -t data/small/en20.200) &> log.sgd.model1.200.txt
 
-(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.model1.full.txt
-(time python featurized_em_wa_mp.py -a LBFGS -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.parallel.model1.full.txt
+(time python featurized_em_wa.py -a LBFGS -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.lbfgs.model1.400.txt
+(time python featurized_em_wa.py -a SGD -m model1 -s data/small/es20.400 -t data/small/en20.400) &> log.sgd.model1.400.txt
