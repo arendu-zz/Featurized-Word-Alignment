115262557cead6475c4055d0257eb40b3b182534
arendu <adithya.r@gmail.com>
2014-11-21 09:16:27 -0500

    first train then print all the scores

--- experiment/run-experiments.sh
+++ experiment/run-experiments.sh
@@ -17,13 +17,9 @@ cat $TARGET_TEST > $TARGET_FULL
 cat $TARGET_TRAIN >> $TARGET_FULL
 
 python initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans -m uniform
-for RC in 0.0 0.01 0.1 0.2
+for RC in 0.001 0.005 0.009
 do
-  time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a 'LBFGS' -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST
-  echo ""
-  echo "*********LBFGS RC:"$RC"********"
-  echo ""
-  python eval_alignment.py $KEY LBFGS.$MODEL.alignments.col
+  time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a 'LBFGS' -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
 done
 
 python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
@@ -32,6 +28,13 @@ echo "********Baseline********"
 echo ""
 python eval_alignment.py $KEY model1.alignments
 
+for RC in 0.001 0.005 0.009
+do
+  echo ""
+  echo "*********LBFGS RC:"$RC"********"
+  echo ""
+  python eval_alignment.py $KEY LBFGS.$RC.$MODEL.alignments.col
+done
 #time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a 'LBFGS' -m $MODEL --iw model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST
 #echo ""
 #echo "*********LBFGS+model1********"
