1a4176d68c884a388381892a34692fd95080c862
Shuoyang Ding <dsy100@gmail.com>
2014-12-01 23:43:05 -0500

    experiment script for real-valued feature

--- experiment/editdistance.py
+++ experiment/editdistance.py
@@ -1,39 +1,57 @@
+# -*- coding: latin1 -*-
 __author__ = 'arenduchintala'
 import numpy as np
+from optparse import OptionParser
 
-def edratio(a,b):
-    ed = editdistance(a,b)
+
+def edratio(a, b):
+    ed = editdistance(a, b)
     edr = ed / float(max(len(a), len(b)))
     return 1.0 - edr
+
+
 def editdistance(a, b):
-    table =  np.zeros((len(a)+1, len(b)+1))
-    for i in range(len(a)+1):
-        table[i,0] = i
-    for j in range(len(b)+1):
-        table[0,j]  = j
-    #print 'start'
-    for i in range(1,len(a)+1):
-        for j in range(1,len(b)+1):
-            if a[i-1] == b[j-1]:
-                table[i,j] = table[i-1, j-1]
+    table = np.zeros((len(a) + 1, len(b) + 1))
+    for i in range(len(a) + 1):
+        table[i, 0] = i
+    for j in range(len(b) + 1):
+        table[0, j] = j
+    # print 'start'
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            if a[i - 1] == b[j - 1]:
+                table[i, j] = table[i - 1, j - 1]
             else:
-                #print i, j
+                # print i, j
                 diag = table[i - 1, j - 1] + 1
-                #print 'diag', diag
+                # print 'diag', diag
                 left = table[i - 1, j] + 1
-                #print 'left', left
+                # print 'left', left
                 top = table[i, j - 1] + 1
-                #print 'top', top
+                # print 'top', top
                 best = min(diag, top, left)
-                #print 'best so far', best
+                # print 'best so far', best
                 table[i, j] = best
-                #print 'current cell', table[i, j]
-    #print table
+                # print 'current cell', table[i, j]
+    # print table
     return table[i, j]
 
 
 if __name__ == "__main__":
-    x = "the"#"ALTRUISM"
-    y = "other"#"PLASMA"
+    x = "de"  # "ALTRUISM"
+    y = "onf"  # "PLASMA"
     ed = editdistance(x, y)
-    #print 'final dist', ed
+    # print 'final dist', ed, 1.0 / (ed + 1.0)
+    edr = edratio(x, y)
+    # print 'final edr', edr
+    # eixit()
+    opt = OptionParser()
+    opt.add_option("-i", dest="basic_features", default="experiment/initial.trans")
+    (options, _) = opt.parse_args()
+
+    for l in open(options.basic_features, 'r').readlines():
+        [t, fr, en, wt] = l.split()
+        ed = str(round(1.0 / (editdistance(fr, en) + 1.0), 4))
+        l2 = '\t'.join([t, fr, en, ed])
+        print l2
+
--- experiment/run-experiments.sh
+++ experiment/run-experiments.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 SOURCE_FULL="source_full.tmp"
 TARGET_FULL="target_full.tmp"
-SOURCE_TRAIN="data/toy.en"
-TARGET_TRAIN="data/toy.fr"
-SOURCE_TEST="data/toy.en"
-TARGET_TEST="data/toy.fr"
-KEY="data/toy.key"
+SOURCE_TRAIN="data/train.en"
+TARGET_TRAIN="data/train.es"
+SOURCE_TEST="data/dev.en"
+TARGET_TEST="data/dev.es"
+KEY="data/dev.key"
 MODEL="model1"
 ALGO="EM"
 echo "training files:" $SOURCE_TRAIN ","  $TARGET_TRAIN
@@ -20,14 +20,20 @@ cat $TARGET_TRAIN >> $TARGET_FULL
 python initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans -m uniform
 python editdistance.py -i initial.trans > initial.feature.values
 for ALGO in "LBFGS" "EM"
-do
-  for RC in 0.0 0.005
+ do
+   for RC in 0.005
   do
-    time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+    time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --fv initial.feature.values
   done
 done
 
-
+# for ALGO in "LBFGS" "EM"
+# do
+#   for RC in 0.0 0.005
+#   do
+#     time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+#   done
+# done
 
 python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
 echo ""
@@ -37,12 +43,13 @@ python eval_alignment.py $KEY model1.alignments
 
 for ALGO in "LBFGS" "EM"
 do
-    for RC in 0.0 0.005
+    for RC in 0.005
     do
       echo ""
       echo "*********"$ALGO " RC:"$RC"********"
       echo ""
-      python eval_alignment.py $KEY $ALGO.$RC.$MODEL.bin.alignments.col
+      # python eval_alignment.py $KEY $ALGO.$RC.$MODEL.bin.alignments.col
+      python eval_alignment.py $KEY $ALGO.$RC.$MODEL.real.alignments.col
     done
 done
 rm $SOURCE_FULL
