ed7648f6174b6424d85ad8519dde6b87764e7891
Shuoyang Ding <dsy100@gmail.com>
2014-12-09 10:08:45 -0500

    code for experiment with ISSAME feature

--- experiment/run-experiments.sh
+++ experiment/run-experiments.sh
@@ -19,21 +19,21 @@ cat $TARGET_TRAIN >> $TARGET_FULL
 
 python initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans -m uniform
 python editdistance.py -i initial.trans > initial.feature.values
-for ALGO in "LBFGS" "EM"
- do
-   for RC in 0.005
-  do
-    time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --fv initial.feature.values
-  done
-done
-
-# for ALGO in "LBFGS" "EM"
-# do
-#   for RC in 0.0 0.005
+# for ALGO in "LBFGS" "EM" "EM-SGD"
+#  do
+#    for RC in 0.0 0.005
 #   do
-#     time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+#     time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --fv initial.feature.values
 #   done
 # done
+
+for ALGO in "LBFGS" "EM" "EM-SGD"
+do
+  for RC in 0.0 0.005
+  do
+    time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+  done
+done
 
 python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
 echo ""
@@ -41,15 +41,15 @@ echo "********Baseline********"
 echo ""
 python eval_alignment.py $KEY model1.alignments
 
-for ALGO in "LBFGS" "EM"
+for ALGO in "LBFGS" "EM" "EM-SGD"
 do
-    for RC in 0.005
+    for RC in 0.0 0.005
     do
       echo ""
       echo "*********"$ALGO " RC:"$RC"********"
       echo ""
-      # python eval_alignment.py $KEY $ALGO.$RC.$MODEL.bin.alignments.col
-      python eval_alignment.py $KEY $ALGO.$RC.$MODEL.real.alignments.col
+      python eval_alignment.py $KEY $ALGO.$RC.$MODEL.bin.alignments.col
+      # python eval_alignment.py $KEY $ALGO.$RC.$MODEL.real.alignments.col
     done
 done
 rm $SOURCE_FULL
