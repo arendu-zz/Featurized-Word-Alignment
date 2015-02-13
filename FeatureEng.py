0c866b9efed0254c5f39327664d1a79d6dd9f58b
arendu <adithya.r@gmail.com>
2015-02-13 17:09:01 -0500

    removed features for pos tagging

--- FeatureEng.py
+++ FeatureEng.py
@@ -1,6 +1,5 @@
 __author__ = 'arenduchintala'
 
-import featurized_em as fe
 import featurized_em_wa_mp as fe_w
 
 global feature_values
@@ -27,10 +26,10 @@ def get_wa_features_fired(type, decision, context):
         fired_features = [(val, (fe_w.E_TYPE, decision, context))]
 
         if decision == context:
-          fired_features += [(1.0, ("IS_SAME", decision, context))]
+            fired_features += [(1.0, ("IS_SAME", decision, context))]
 
-        #if context == fe_w.NULL:
-        #    fired_features += [(-1.0, ("IS_FROM_NULL", context))]
+        # if context == fe_w.NULL:
+        # fired_features += [(-1.0, ("IS_FROM_NULL", context))]
 
         if fe_w.has_pos:
             decision_pos = decision.split("_")[1]
@@ -51,20 +50,3 @@ def get_wa_features_fired(type, decision, context):
     return fired_features
 
 
-def get_pos_features_fired(type, decision, context):
-    if decision is not fe.BOUNDARY_STATE and context is not fe.BOUNDARY_STATE:
-        if type == fe.E_TYPE:
-            # suffix feature, prefix feature
-            fired_features = list(set([(fe.E_TYPE_SUF, str(decision[-3:]).lower(), context),
-                                       (fe.E_TYPE_PRE, str(decision[:2]).lower(), context),
-                                       (fe.E_TYPE_PRE, str(decision[:1]).lower(), context),
-                                       ('IS_CAP', str(decision[0]).isupper(), context),
-                                       ('IS_ALNUM', str(decision).isalnum(), context),
-                                       ('IS_SAME', decision == context, context),
-                                       (fe.E_TYPE, decision, context)]))
-            # fired_features = [(fe.E_TYPE, decision, context)]
-            return fired_features
-        elif type == fe.T_TYPE:
-            return [(fe.T_TYPE, decision, context)]
-    else:
-        return [(type, decision, context)]
