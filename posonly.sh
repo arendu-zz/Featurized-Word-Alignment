ed57a7a08accc8a3a286e49cdfd17a956ae52139
arendu <adithya.r@gmail.com>
2015-02-12 21:32:23 -0500

    deleted unwanted files

--- posonly.sh
+++
@@ -1,7 +0,0 @@
-for LANG in "en" "es"
-do
-for WP in train dev dev.small
-do
-    python pos.py -p experiment/data/${WP}.${LANG}.pos -m experiment/data/${LANG}.map -l ${LANG} > experiment/data/${WP}.${LANG}.only.pos
-done
-done
