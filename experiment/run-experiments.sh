3da4d0e5457bec62c6308e49603c7cf0c2838482
arendu <adithya.r@gmail.com>
2015-02-12 21:16:23 -0500

    added sh experiments

--- experiment/run-experiments.sh
+++ experiment/run-experiments.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 SOURCE_FULL="source_full.tmp"
 TARGET_FULL="target_full.tmp"
-SOURCE_TRAIN="data/dev.en"
-TARGET_TRAIN="data/dev.es"
+SOURCE_TRAIN="data/train.en"
+TARGET_TRAIN="data/train.es"
 SOURCE_TEST="data/dev.en"
 TARGET_TEST="data/dev.es"
 KEY="data/dev.key"
-MODEL="model1"
-ALGO="EM"
 echo "training files:" $SOURCE_TRAIN ","  $TARGET_TRAIN
 echo "testing  files:" $SOURCE_TEST "," $TARGET_TEST
 touch $SOURCE_FULL
@@ -19,14 +17,12 @@ cat $TARGET_TRAIN >> $TARGET_FULL
 
 python initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans -m uniform
 python editdistance.py -i initial.trans > initial.feature.values
-for ALGO in "LBFGS" 
+for ALGO in  "LBFGS" "EM"
 do
-    for RC in 0.0 0.005
+    for RC in 0.0
     do
         #time python ../featurized_em_wa.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
-        #time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
-        time python ../featurized_fast_align.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
-        #time python ../featurized_model1_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+        time python ../featurized_em_wa_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO -m $MODEL --iw initial.trans.log --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
     done
 done
 
@@ -40,26 +36,23 @@ done
 
 
 
-time python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
+python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
 echo ""
 echo "********Baseline********"
 echo ""
 python eval_alignment.py $KEY model1.alignments
 
-for ALGO in "LBFGS"
+for ALGO in "LBFGS" "EM"
 do
-    for RC in 0.0 0.005
+    for RC in 0.0
     do
-        for MODEL in  "simple-model1"
-        do
         echo "" 
-        echo "*********"$ALGO " " $MODEL "  RC:"$RC"********"
+        echo "*********"$ALGO " RC:"$RC"********"
         echo ""
-        python eval_alignment.py $KEY sp.$ALGO.$RC.$MODEL.bin.alignments.col
-        #python eval_alignment.py $KEY mp.$ALGO.$RC.$MODEL.bin.alignments.col
+        #python eval_alignment.py $KEY $ALGO.$RC.$MODEL.bin.alignments.col
+        python eval_alignment.py $KEY mp.$ALGO.$RC.$MODEL.bin.alignments.col
         #python eval_alignment.py $KEY $ALGO.$RC.$MODEL.real.alignments.col
     done
-done
 done
 rm $SOURCE_FULL
 rm $TARGET_FULL 
---
+++ experiment/run-fast-experiment.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+SOURCE_FULL="source_full.tmp"
+TARGET_FULL="target_full.tmp"
+SOURCE_TRAIN="data/dev.small.en"
+TARGET_TRAIN="data/dev.small.es"
+SOURCE_TEST="data/dev.small.en"
+TARGET_TEST="data/dev.small.es"
+KEY="data/dev.small.key"
+MODEL="fast-model1"
+echo "training files:" $SOURCE_TRAIN ","  $TARGET_TRAIN
+echo "testing  files:" $SOURCE_TEST "," $TARGET_TEST
+touch $SOURCE_FULL
+touch $TARGET_FULL
+cat $SOURCE_TEST > $SOURCE_FULL
+cat $SOURCE_TRAIN >> $SOURCE_FULL
+cat $TARGET_TEST > $TARGET_FULL
+cat $TARGET_TRAIN >> $TARGET_FULL
+
+python initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans -m uniform
+#python editdistance.py -i initial.trans > initial.feature.values
+for ALGO in "EM" 
+do
+    for RC in 0.0 
+    do
+        time python ../featurized_fast_align_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --iw initial.trans --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC
+        echo "."
+    done
+done
+
+
+time python model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments -as $SOURCE_TEST -at $TARGET_TEST
+echo ""
+echo "********Baseline********"
+echo ""
+python eval_alignment.py $KEY model1.alignments
+for MODEL in "fast-model1"
+do
+for ALGO in "EM" 
+do
+    for RC in 0.0 
+    do
+        echo ""
+        echo "********* MODEL:"$MODEL" ALGO:"$ALGO " RC:"$RC"********"
+        echo ""
+        python eval_alignment.py $KEY mp.$ALGO.$RC.$MODEL.bin.alignments.col
+    done
+done
+done
+rm $SOURCE_FULL
+rm $TARGET_FULL 
