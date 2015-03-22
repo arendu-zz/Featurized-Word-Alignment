6319956908372fd4cdf095611ab28d538bdafcf0
Adithya Renduchintala <arenduc1@login.clsp.jhu.edu>
2015-03-22 17:24:52 -0400

    run hybrid using qsub

--- experiment/run-hybrid-experiment.sh
+++ experiment/run-hybrid-experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-source ./config.cfg
+source /home/arenduc1/MachineTranslation/Featurized-EM/experiment/config.cfg
 SOURCE_FULL="source_full.tmp"
 TARGET_FULL="target_full.tmp"
 DICT_PATH="data/dictionary_features.es-en"
@@ -14,15 +14,15 @@ cat $SOURCE_TRAIN >> $SOURCE_FULL
 cat $TARGET_TEST > $TARGET_FULL
 cat $TARGET_TRAIN >> $TARGET_FULL
 
-python ${CODE_DIR}/initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o initial.trans
-time python ${CODE_DIR}/model1.py -s $SOURCE_FULL -t $TARGET_FULL -i initial.trans -p model1.probs -a model1.alignments --as $SOURCE_TEST --at $TARGET_TEST
+python ${CODE_DIR}/initial_translation.py  -s $SOURCE_FULL -t $TARGET_FULL  -o $EXPERIMENTS_DIR/initial.trans
+time python ${CODE_DIR}/model1.py -s $SOURCE_FULL -t $TARGET_FULL -i $EXPERIMENTS_DIR/initial.trans -p $EXPERIMENTS_DIR/model1.probs -a $EXPERIMENTS_DIR/model1.alignments --as $SOURCE_TEST --at $TARGET_TEST
 for ALGO in "LBFGS" 
 do
     for RC in 0.0
     do
-        time python ${CODE_DIR}/use_hm1.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH --oa use
-        time python ${CODE_DIR}/hybrid_model1.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH
-        time python ${CODE_DIR}/hybrid_model1_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH
+        time python ${CODE_DIR}/use_hm1.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 $EXPERIMENTS_DIR/model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH --oa use
+        #time python ${CODE_DIR}/hybrid_model1.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 $EXPERIMENTS_DIR/model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH
+        #time python ${CODE_DIR}/hybrid_model1_mp.py -s $SOURCE_FULL -t $TARGET_FULL -a $ALGO  --m1 $EXPERIMENTS_DIR/model1.probs --ts $SOURCE_TEST --tt $TARGET_TEST -r $RC --df $DICT_PATH
         echo "."
     done
 done
@@ -32,7 +32,7 @@ done
 echo ""
 echo "********Baseline********"
 echo ""
-python ${TOOLS_DIR}/eval_alignment.py $KEY model1.alignments
+python ${TOOLS_DIR}/eval_alignment.py $KEY $EXPERIMENTS_DIR/model1.alignments
 
 for ALGO in "LBFGS" 
 do
@@ -41,16 +41,16 @@ do
         echo ""
         echo "*********SIMPLE "$ALGO " RC:"$RC"********"
         echo ""
-        python ${TOOLS_DIR}/eval_alignment.py $KEY sp.$ALGO.$RC.$MODEL.bin.use.col
-        python ${TOOLS_DIR}/eval_alignment.py $KEY sp.$ALGO.$RC.$MODEL.bin.alignments.col
-        python ${TOOLS_DIR}/eval_alignment.py $KEY mp.$ALGO.$RC.$MODEL.bin.alignments.col
+        python ${TOOLS_DIR}/eval_alignment.py $KEY $EXPERIMENTS_DIR/sp.$ALGO.$RC.$MODEL.bin.use.col
+        #python ${TOOLS_DIR}/eval_alignment.py $KEY $EXPERIMENTS_DIR/sp.$ALGO.$RC.$MODEL.bin.alignments.col
+        #python ${TOOLS_DIR}/eval_alignment.py $KEY mp.$ALGO.$RC.$MODEL.bin.alignments.col
     done
 done
 echo "deleting files..."
 rm $SOURCE_FULL
 rm $TARGET_FULL
-rm initial.trans
-rm model1.probs
-rm model1.alignments
-rm mp.*
-rm sp.*
+rm $EXPERIMENTS_DIR/initial.trans
+rm $EXPERIMENTS_DIR/model1.probs
+rm $EXPERIMENTS_DIR/model1.alignments
+#rm $EXPERIMENTS_DIR/mp.*
+#rm $EXPERIMENTS_DIR/sp.*
